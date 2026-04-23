75602526be598ac8a945cba29a42c07b7ba3bac7
iananalistati-sketch <ian.analistati@gmail.com>
2026-04-23 09:50:58 -0300

    Update data.ts

--- api/data.ts
+++ api/data.ts
@@ -1,13 +1,14 @@
 import { google } from "googleapis";
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   try {
-    const auth = new google.auth.JWT(
-      process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      undefined,
-      process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      ["https://www.googleapis.com/auth/spreadsheets"]
-    );
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
 
     const sheets = google.sheets({ version: "v4", auth });
 
@@ -19,8 +20,8 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json(response.data.values);
-  } catch (error) {
-    console.error(error);
+  } catch (error: any) {
+    console.error("ERRO API:", error);
     res.status(500).json({ error: error.message });
   }
 }
