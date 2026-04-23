eee70737aeaa713c40fd04bed207e52fd89010d6
iananalistati-sketch <ian.analistati@gmail.com>
2026-04-23 09:41:08 -0300

    Update data.ts

--- api/data.ts
+++ api/data.ts
@@ -4,20 +4,23 @@ export default async function handler(req, res) {
   try {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      undefined,
+      process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       ["https://www.googleapis.com/auth/spreadsheets"]
     );
 
     const sheets = google.sheets({ version: "v4", auth });
 
+    const sheet = req.query.sheet || "tbChamadosMV";
+
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: process.env.SHEET_NAME || "tbChamadosMV",
+      range: sheet,
     });
 
     res.status(200).json(response.data.values);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 }
