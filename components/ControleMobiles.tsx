3b2edc99f69b8610c8190b2835bd201787e83371
iananalistati-sketch <ian.analistati@gmail.com>
2026-09-08 14:28:23 -0300

    Integra configuração aos formulários de mobiles

--- components/ControleMobiles.tsx
+++ components/ControleMobiles.tsx
@@ -381,8 +381,8 @@ export default function ControleMobiles({
       </div>}
 
       <DetalhesAppsMobileModal isOpen={viewingAppsRow !== null} coletor={viewingAppsColetor} appRows={viewingApps} mobileApps={mobileApps} mobileConfig={mobileConfig} currentUserName={currentUserName} canEdit={canEdit} onSave={onSaveMobileApp} onClose={() => setViewingAppsRow(null)} />
-      <EditarMobileModal isOpen={editingRow !== null} row={editingRow} headers={headers} allRows={rows} currentUserName={currentUserName} normalize={normalize} onClose={handleCloseEdit} onSave={onSaveRow} onRefresh={onRefresh} />
-      <NovoMobileModal isOpen={showCreateModal} headers={headers} allRows={rows} normalize={normalize} onClose={() => setShowCreateModal(false)} onCreate={onCreateRow} />
+      <EditarMobileModal isOpen={editingRow !== null} row={editingRow} headers={headers} allRows={rows} mobileConfig={mobileConfig} mobileApps={mobileApps} currentUserName={currentUserName} normalize={normalize} onClose={handleCloseEdit} onSave={onSaveRow} onSaveMobileApp={onSaveMobileApp} onRefresh={onRefresh} />
+      <NovoMobileModal isOpen={showCreateModal} headers={headers} allRows={rows} mobileConfig={mobileConfig} currentUserName={currentUserName} normalize={normalize} onClose={() => setShowCreateModal(false)} onCreate={onCreateRow} />
       <AtualizacaoLoteMobilesModal isOpen={showBulkUpdateModal} selectedCount={selectedRows.length} currentUserName={currentUserName} onClose={() => setShowBulkUpdateModal(false)} onApply={handleBulkUpdate} />
     </div>
   );
